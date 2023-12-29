675d6f6cdd2095189211f3e8b958f4951d68bb9e
VirtualJSW <79466456+VirtualJSW@users.noreply.github.com>
2023-12-29 15:28:28 +0000

    JSW Pilot and Squadron Update
    Updated all JSW Pilots and Squadrons to reflect Discord roles.

--- data/squadrons.lua
+++ data/squadrons.lua
@@ -20,9 +20,11 @@ return {
         pilots = {
             "ea2dca05dc204673da916448f77f00f1", -- Engines
             "c82a053e0cc44ca3fafbcd435609b6e9", -- Duke
+			"67685fc9d16c0e4701d43a82ecb4ca82", -- Slippy			
             "d4dfacf7d87e9a24c1f705bb886ab0dd", -- Pete G
             "8433bf314f50b089c4a8fb0e540c2698", -- Ganduwulf
             "d30a8bc09f9e453798bfc601e9f607c6", -- Madog
+			"8ee67d60c11aede8fed0bb82b90072ce", --Smythes
         }
     },
     {
@@ -37,33 +39,60 @@ return {
         co = "d924195f7dfe499a192c11f00489df4a", -- Mailman
         pilots = {
             "d924195f7dfe499a192c11f00489df4a", -- Mailman
+			"67685fc9d16c0e4701d43a82ecb4ca82", -- Slippy			
+			"e2d4db8d51cd658e68efd2c36a4dc7e7", -- Sideburns
             "813759da956084866e9e72540e544b68", -- Razor
-            "891c8993e8fdbcb509aa0dbe2c9fe43e", -- Shakes
-            "67685fc9d16c0e4701d43a82ecb4ca82", -- Slippy
+            "891c8993e8fdbcb509aa0dbe2c9fe43e", -- Shakespeade            
             "880046c63693ceeee0727f5e56209ae4", -- Scooby
             "33088421d26097eef47da9a002488516", -- Spanner
             "eba02d205566d321799ab513188ddcd7", -- Mort
             "168ad57dbcf420e400e1175b6b461974", -- Obic
-            "66e4049ecc6c5fe805362d0088023edf", -- Funky
+            "66e4049ecc6c5fe805362d0088023edf", -- FunkySchnitzel
             "fd287e3dabf2b4645499972792ebba30", -- Steph
+			"606b3219538f69a2c4571c9b8e34eab3", -- Direful Tiger
+			"e97263dc4e93acc39e37d4b76054c16c", -- Swift One
+			"339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus			
         }
     },
     {
         name = "656 Sqn",
         motto = "Volans et videns",
         type = "AH-64",
-        pseudoType = "Apache AH-64E",
+        pseudoType = "Apache AH-1",
         aircraft = {
             {tailNumber = "ZA123", type = "FGR1", status = "Serviceable"},
         },
         co = "88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal
         pilots = {
             "88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal
+			"02decd2d485c2c35a8e817dc751951d5", -- Buck
             "3316bca529c797f031d8c54c14a0f5d3", -- Wee Neal
-            "1a1cba06dd0066c82910007ab36a3c1f", -- Torchy
+            "1a1cba06dd0066c82910007ab36a3c1f", -- Torchy			
             "915811de9b95ea8f1496085b5a833cb5", -- Skuba
             "e698907134a269f5ee2c4155cb589540", -- DJ
-            "7567ee520811f12f54cd2471c4ff0016", -- Cowan
+			"4735c8e0dedb622c1f850dbb78470d20", -- Carto
+			"e473f6817732c53fd33b3b5b923bb3b8", -- Dangerman			
+			"ebfce242af88d0a64dd8820ece14708e", -- Jester
+			"ea2dca05dc204673da916448f77f00f1", -- Engines	
+			"520d768a67604b6328ecc5c6cd829fc5", -- Falcon
+			"803abf3c05cfa1b2eb7eaf194303b0ef", -- Action Hero
+			"fc7eaf2e8fdf3fdf85fd150d8d31117a", -- Casper
+			"7567ee520811f12f54cd2471c4ff0016", -- Cowan
+			"c515900aec81f4de675708369df6db7b", -- DannyR
+			"0ed16e2fab622be7be70038750d3f3bf", -- Fisher
+			"34c71ada8848ff2a30d8f2631cdaf664", -- Houseman
+			"31f79da84f0afe4a37b7db5e544184ef", -- Lenny
+			"25e873c6f2e21552e7eb837adf496179", -- MiMiKiwi
+			"6c977c9fc7ed1cdceb2ab823f0e0dab9", -- Phoenix
+			"779a2c65fbebc6abc99f9dace3a68af8", -- Podo
+			"05be445e57c28e2e19b5b90487252fd1", -- Solex
+			"dacaba8cb4b14d78ed84e897ed653eb3", -- Varry
+			"a7eed3e754fa61f3546530559dc87e18", -- Venom
+			"30f30ebc24983a7a35d59d2ee6034513", -- Yank
+			"e97263dc4e93acc39e37d4b76054c16c", -- Swift One
+			"339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus	
+			"880046c63693ceeee0727f5e56209ae4", -- Scooby
+			"269afd7b2f4120a1580230699c6741d3", -- Monkey"			
         }
     },
     {
@@ -81,9 +110,15 @@ return {
             "6b7faedbf5e4500cda9efe482cf5d764", -- Cactus
             "534894c6525946fcf1ba1b7f55233590", -- Poacher
             "880046c63693ceeee0727f5e56209ae4", -- Scooby
-            "b564266749ac14a3fe3e5df7d29cf243", -- Blythe
+            "b564266749ac14a3fe3e5df7d29cf243", -- Blythee
             "1ae5c656623ee0792397970e46ed00ad", -- Hawk
             "520d768a67604b6328ecc5c6cd829fc5", -- Falcon
+			"6f784cb7b82e58ea996432664a6d9ba8", -- Jez
+			"7a42b9d3472eea06d42ba54ed7f4c4a9", -- Flynn
+			"2d4687a1e8563df17d6e7ba5abb0aeeb", -- Jethro
+			"abcd66987f65616ec5f5cfecb18f7c16", -- MajorBloodnok
+			"63620908d21196f4a0fc3d1229e8f80a", -- NightGarter
+			"f94e59d80a9b9da02ff078db283f6b75", -- Yuri
         }
     },    
     {
@@ -98,10 +133,17 @@ return {
         pilots = {
             "e473f6817732c53fd33b3b5b923bb3b8", -- Dangerman
             "813759da956084866e9e72540e544b68", -- Razor
+			"d924195f7dfe499a192c11f00489df4a", -- Mailman
+			"88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal
             "339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus
             "e97263dc4e93acc39e37d4b76054c16c", -- Swift One
             "db0d1a3afa403f1e41991e71ebd9f384", -- Christian
             "e5ad0b08b97038d7c064b3d64943a324", -- Liminal
+			"3316bca529c797f031d8c54c14a0f5d3", -- Wee Neal
+			"ebfce242af88d0a64dd8820ece14708e", -- Jester	
+			"269afd7b2f4120a1580230699c6741d3", -- Monkey"
+			"66e4049ecc6c5fe805362d0088023edf", -- FunkySchnitzel
+			"fd287e3dabf2b4645499972792ebba30", -- Steph
         }
     },
     {
@@ -117,9 +159,20 @@ return {
             "6b7faedbf5e4500cda9efe482cf5d764", -- Cactus
             "534894c6525946fcf1ba1b7f55233590", -- Poacher
             "c153e49b73efbf650cd11ac76f3831df", -- RJF
+			"7a42b9d3472eea06d42ba54ed7f4c4a9", -- Flynn			
+			"02decd2d485c2c35a8e817dc751951d5", -- Buck	
+			"88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal			
             "366f6f18abaafd5dc38ca634655f462f", -- Puddles
             "a450226ca342ba4235e5734f73989e9e", -- Jxckmc
             "7248624f73137d934947745d6379c93a", -- Spectre
+			"269afd7b2f4120a1580230699c6741d3", -- Monkey"
+			"2d65ee9349e5cd50088b83a75a26c53d", -- Ripper66
+			"0ed16e2fab622be7be70038750d3f3bf", -- Fisher
+			"34c71ada8848ff2a30d8f2631cdaf664", -- Houseman
+			"3316bca529c797f031d8c54c14a0f5d3", -- Wee Neal
+			"db0d1a3afa403f1e41991e71ebd9f384", -- Christian
+			"6f784cb7b82e58ea996432664a6d9ba8", -- Jez
+			"1ae5c656623ee0792397970e46ed00ad", -- Hawk			
         }
     },
 }
