eb1f2c1bdce897d51d464fc605a8ddcc8eaf6a90
Gavin Edwards <gavine@sentientvision.com>
2023-12-29 16:01:21 +0000

    Reordering pilots.

--- data/squadrons.lua
+++ data/squadrons.lua
@@ -39,19 +39,19 @@ return {
         co = "d924195f7dfe499a192c11f00489df4a", -- Mailman
         pilots = {
             "d924195f7dfe499a192c11f00489df4a", -- Mailman
+            "813759da956084866e9e72540e544b68", -- Razor
 			"67685fc9d16c0e4701d43a82ecb4ca82", -- Slippy			
 			"e2d4db8d51cd658e68efd2c36a4dc7e7", -- Sideburns
-            "813759da956084866e9e72540e544b68", -- Razor
             "891c8993e8fdbcb509aa0dbe2c9fe43e", -- Shakespeade            
+			"e97263dc4e93acc39e37d4b76054c16c", -- Swift One
             "880046c63693ceeee0727f5e56209ae4", -- Scooby
+			"339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus			
             "33088421d26097eef47da9a002488516", -- Spanner
             "eba02d205566d321799ab513188ddcd7", -- Mort
             "168ad57dbcf420e400e1175b6b461974", -- Obic
+			"606b3219538f69a2c4571c9b8e34eab3", -- Direful Tiger
             "66e4049ecc6c5fe805362d0088023edf", -- FunkySchnitzel
             "fd287e3dabf2b4645499972792ebba30", -- Steph
-			"606b3219538f69a2c4571c9b8e34eab3", -- Direful Tiger
-			"e97263dc4e93acc39e37d4b76054c16c", -- Swift One
-			"339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus			
         }
     },
     {
@@ -65,16 +65,18 @@ return {
         co = "88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal
         pilots = {
             "88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal
+			"e473f6817732c53fd33b3b5b923bb3b8", -- Dangerman			
 			"02decd2d485c2c35a8e817dc751951d5", -- Buck
+			"339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus	
             "3316bca529c797f031d8c54c14a0f5d3", -- Wee Neal
             "1a1cba06dd0066c82910007ab36a3c1f", -- Torchy			
+			"e97263dc4e93acc39e37d4b76054c16c", -- Swift One
+			"880046c63693ceeee0727f5e56209ae4", -- Scooby
+			"520d768a67604b6328ecc5c6cd829fc5", -- Falcon
+			"ebfce242af88d0a64dd8820ece14708e", -- Jester
             "915811de9b95ea8f1496085b5a833cb5", -- Skuba
             "e698907134a269f5ee2c4155cb589540", -- DJ
 			"4735c8e0dedb622c1f850dbb78470d20", -- Carto
-			"e473f6817732c53fd33b3b5b923bb3b8", -- Dangerman			
-			"ebfce242af88d0a64dd8820ece14708e", -- Jester
-			"ea2dca05dc204673da916448f77f00f1", -- Engines	
-			"520d768a67604b6328ecc5c6cd829fc5", -- Falcon
 			"803abf3c05cfa1b2eb7eaf194303b0ef", -- Action Hero
 			"fc7eaf2e8fdf3fdf85fd150d8d31117a", -- Casper
 			"7567ee520811f12f54cd2471c4ff0016", -- Cowan
@@ -89,9 +91,6 @@ return {
 			"dacaba8cb4b14d78ed84e897ed653eb3", -- Varry
 			"a7eed3e754fa61f3546530559dc87e18", -- Venom
 			"30f30ebc24983a7a35d59d2ee6034513", -- Yank
-			"e97263dc4e93acc39e37d4b76054c16c", -- Swift One
-			"339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus	
-			"880046c63693ceeee0727f5e56209ae4", -- Scooby
 			"269afd7b2f4120a1580230699c6741d3", -- Monkey"			
         }
     },
@@ -110,10 +109,10 @@ return {
             "6b7faedbf5e4500cda9efe482cf5d764", -- Cactus
             "534894c6525946fcf1ba1b7f55233590", -- Poacher
             "880046c63693ceeee0727f5e56209ae4", -- Scooby
+			"6f784cb7b82e58ea996432664a6d9ba8", -- Jez
             "b564266749ac14a3fe3e5df7d29cf243", -- Blythee
             "1ae5c656623ee0792397970e46ed00ad", -- Hawk
             "520d768a67604b6328ecc5c6cd829fc5", -- Falcon
-			"6f784cb7b82e58ea996432664a6d9ba8", -- Jez
 			"7a42b9d3472eea06d42ba54ed7f4c4a9", -- Flynn
 			"2d4687a1e8563df17d6e7ba5abb0aeeb", -- Jethro
 			"abcd66987f65616ec5f5cfecb18f7c16", -- MajorBloodnok
@@ -136,11 +135,11 @@ return {
 			"d924195f7dfe499a192c11f00489df4a", -- Mailman
 			"88dae793ecd2d2ed030fbd6bca2297f7", -- Gimbal
             "339df0e69cc97e60f2befbcfbdcfcee2", -- Bogus
+			"3316bca529c797f031d8c54c14a0f5d3", -- Wee Neal
             "e97263dc4e93acc39e37d4b76054c16c", -- Swift One
             "db0d1a3afa403f1e41991e71ebd9f384", -- Christian
-            "e5ad0b08b97038d7c064b3d64943a324", -- Liminal
-			"3316bca529c797f031d8c54c14a0f5d3", -- Wee Neal
 			"ebfce242af88d0a64dd8820ece14708e", -- Jester	
+            "e5ad0b08b97038d7c064b3d64943a324", -- Liminal
 			"269afd7b2f4120a1580230699c6741d3", -- Monkey"
 			"66e4049ecc6c5fe805362d0088023edf", -- FunkySchnitzel
 			"fd287e3dabf2b4645499972792ebba30", -- Steph
